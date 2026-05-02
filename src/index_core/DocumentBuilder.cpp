28504a8e474e4f4b4ebdf49561c420e1089f806a
iaroslav <antipovyaroslav382@gmail.com>
2026-05-02 13:30:48 +0300

    lab5done24

--- src/index_core/DocumentBuilder.cpp
+++ src/index_core/DocumentBuilder.cpp
@@ -1,13 +1,21 @@
 #include "DocumentBuilder.hpp"
 #include <algorithm>
 
+static bool isDelimiter(char c)
+{
+    return std::isspace(static_cast<unsigned char>(c)) || c == ',' || c == '.' || c == '!' || c == '?' || c == ';' ||
+           c == ':' || c == '-' || c == '_' || c == '(' || c == ')' || c == '[' || c == ']' || c == '{' || c == '}' ||
+           c == '<' || c == '>' || c == '"' || c == '\'' || c == '/' || c == '\\';
+}
+
 std::vector<std::string_view> DocumentBuilder::splitWords(std::string_view text)
 {
     std::vector<std::string_view> words;
     size_t start = 0;
+
     while (start < text.size())
     {
-        while (start < text.size() && std::isspace(static_cast<unsigned char>(text[start])))
+        while (start < text.size() && isDelimiter(text[start]))
         {
             ++start;
         }
@@ -15,12 +23,12 @@ std::vector<std::string_view> DocumentBuilder::splitWords(std::string_view text)
             break;
 
         size_t end = start;
-        while (end < text.size() && !std::isspace(static_cast<unsigned char>(text[end])))
+        while (end < text.size() && !isDelimiter(text[end]))
         {
             ++end;
         }
-        words.emplace_back(text.substr(start, end - start));
 
+        words.emplace_back(text.substr(start, end - start));
         start = end;
     }
     return words;
